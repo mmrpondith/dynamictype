4c76fc9b9daed49aaf2a1ec2e6947b52ae60208a
“rahulislamitibd” <itibdd@gmail.com>
2021-02-12 10:01:52 +0600

    dynamictype.go

---
+++ dynamictype.go
@@ -0,0 +1,18 @@
+package main 
+import "fmt"
+func main(){
+/*
+miskt type and dainamic
+*/
+//var a= "tomar name "
+//var b=524
+//var c=32.555
+var a, b, c="toamr name ki",1542,25.255
+fmt.Println(a)
+fmt.Println(b)
+fmt.Println (c)
+fmt.Printf("a is one kinds of %T\n",a)
+fmt.Printf("b is one kinds of %T\n",b)
+fmt.Printf("c is one kinds of %T\n",c)
+
+}
